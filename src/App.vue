509fe8e435f8c4bbc40a8f287dec2d72364069a0
Stroev D <stroev.core@gmail.com>
2023-07-05 14:48:14 +0300

    prettier set up, code formatted

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,84 @@
 <template>
-    <main>
-        <header class="header">
-            <img src="/logo.svg" alt="Logo" class="header__logo">
-            <h2>My favorite movies</h2>
-        </header>
-        <div class="tabs">
-            <button
-                :class="['btn', {btn_green: movieStore.activeTab === 1}]"
-                @click="setTab(1)"
-            >Favorite</button>
-            <button
-                :class="['btn', {btn_green: movieStore.activeTab === 2}]"
-                @click="setTab(2)"
-            >Search</button>
-        </div>
-        <div class="movies" v-if="movieStore.activeTab === 1">
-            <div>
-                <h3>Watched movies</h3>
-                <Movie
-                    v-for="movie of movieStore.watchedMovies"
-                    :key="movie.id"
-                    :movie="movie"
-                />
-            </div>
-            <h3>All movies (count: {{ movieStore.totalMoviesCounter }})</h3>
-            <Movie
-                v-for="movie of movieStore.movies"
-                :key="movie.id"
-                :movie="movie"
-            />
-        </div>
-        <div class="search" v-else>Search</div>
-    </main>
+  <main>
+    <header class="header">
+      <img src="/logo.svg" alt="Logo" class="header__logo" />
+      <h2>My favorite movies</h2>
+    </header>
+    <div class="tabs">
+      <button
+        :class="['btn', { btn_green: movieStore.activeTab === 1 }]"
+        @click="setTab(1)"
+      >
+        Favorite
+      </button>
+      <button
+        :class="['btn', { btn_green: movieStore.activeTab === 2 }]"
+        @click="setTab(2)"
+      >
+        Search
+      </button>
+    </div>
+    <div class="movies" v-if="movieStore.activeTab === 1">
+      <div>
+        <h3>Watched movies</h3>
+        <Movie
+          v-for="movie of movieStore.watchedMovies"
+          :key="movie.id"
+          :movie="movie"
+        />
+      </div>
+      <h3>All movies (count: {{ movieStore.totalMoviesCounter }})</h3>
+      <Movie
+        v-for="movie of movieStore.movies"
+        :key="movie.id"
+        :movie="movie"
+      />
+    </div>
+    <div class="search" v-else>Search</div>
+  </main>
 </template>
 
 <script setup lang="ts">
-import { useMovieStore } from './stores/MovieStore';
-import Movie from './components/Movie.vue';
+import { useMovieStore } from "./stores/MovieStore";
+import Movie from "./components/Movie.vue";
 
-const setTab = (id:number) => {
-    movieStore.setActiveTab(id);
-}
+const setTab = (id: number) => {
+  movieStore.setActiveTab(id);
+};
 
-const movieStore = useMovieStore()
+const movieStore = useMovieStore();
 </script>
 
 <style scoped>
-.header{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 10px;
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
 }
-.header__logo{
-    max-width: 50px;
-    margin-right: 20px;
+.header__logo {
+  max-width: 50px;
+  margin-right: 20px;
 }
 .btn {
-    border: none;
-    width: 100px;
-    height: 40px;
-    font-size: 14px;
-    margin: 0 10px;
-    border-radius: 10px;
-    cursor: pointer;
-    background-color: #cacaca;
+  border: none;
+  width: 100px;
+  height: 40px;
+  font-size: 14px;
+  margin: 0 10px;
+  border-radius: 10px;
+  cursor: pointer;
+  background-color: #cacaca;
 }
-.btn:hover{
-    opacity: .7;
-    
+.btn:hover {
+  opacity: 0.7;
 }
-.btn_green{
-    background-color: #69cd69;
+.btn_green {
+  background-color: #69cd69;
 }
-.tabs{
-    display: flex;
-    justify-content: center;
-    margin-bottom: 30px;
+.tabs {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
 }
 </style>
--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,107 +1,106 @@
 <template>
-    <div class="movie">
-        <img
-            :src="`https://image.tmdb.org/t/p/w300_and_h450_bestv2${movie.poster_path}`"
-            :alt="movie.original_title"
-            class="movie__img">
-        <div>
-            <div class="movie__name">
-                {{ movie.original_title }} ({{ movie.release_date }})
-            </div>
-            <span class="movie__overview">{{ movie.overview }}</span>
-            <div class="movie__buttons">
-                <button
-                    class="movie__button movie__button-watched"
-                    @click="movieStore.toggleWatch(movie.id)"
-                >
-                    <span v-if="!movie.isWatched">Watched</span>
-                    <span v-else>Unwatched</span>
-                </button>
-                <button
-                    class="movie__button movie__button-delete"
-                    @click="movieStore.deleteMovie(movie.id)"
-                >Delete</button>
-            </div>
-        </div>
+  <div class="movie">
+    <img
+      :src="`https://image.tmdb.org/t/p/w300_and_h450_bestv2${movie.poster_path}`"
+      :alt="movie.original_title"
+      class="movie__img"
+    />
+    <div>
+      <div class="movie__name">
+        {{ movie.original_title }} ({{ movie.release_date }})
+      </div>
+      <span class="movie__overview">{{ movie.overview }}</span>
+      <div class="movie__buttons">
+        <button
+          class="movie__button movie__button-watched"
+          @click="movieStore.toggleWatch(movie.id)"
+        >
+          <span v-if="!movie.isWatched">Watched</span>
+          <span v-else>Unwatched</span>
+        </button>
+        <button
+          class="movie__button movie__button-delete"
+          @click="movieStore.deleteMovie(movie.id)"
+        >
+          Delete
+        </button>
+      </div>
     </div>
-    
+  </div>
 </template>
 
-<script
-    lang="ts"
-    setup
->
-import { useMovieStore } from '../stores/MovieStore';
+<script lang="ts" setup>
+import { useMovieStore } from "../stores/MovieStore";
 
 const movieStore = useMovieStore();
 
 const props = defineProps({
-    movie: {
-        type: Object,
-        required: true,
-        default: () => {}
-    }
-})
+  movie: {
+    type: Object,
+    required: true,
+    default: () => {},
+  },
+});
 </script>
 
 <style scoped>
 .movie {
-    display: grid;
-    grid-template-columns: 200px 1fr;
-    column-gap: 30px;
-    margin-bottom: 20px;
-    border: 2px solid #efefef;
-    padding: 10px;
-    border-radius: 10px;
+  display: grid;
+  grid-template-columns: 200px 1fr;
+  column-gap: 30px;
+  margin-bottom: 20px;
+  border: 2px solid #efefef;
+  padding: 10px;
+  border-radius: 10px;
 }
 
 .movie__accept {
-    margin-right: 10px;
+  margin-right: 10px;
 }
 
 .movie__img {
-    width: 200px;
-    height: 200px;
-    object-fit: cover;
-    border-radius: 50%;
+  width: 200px;
+  height: 200px;
+  object-fit: cover;
+  border-radius: 50%;
 }
 
 .movie__name {
-    display: flex;
-    align-items: center;
-    font-size: 20px;
-    margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+  font-size: 20px;
+  margin-bottom: 20px;
 }
 
 .movie__overview {
-    display: block;
-    margin-bottom: 20px;
+  display: block;
+  margin-bottom: 20px;
 }
 
 .movie__buttons {
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-.movie__button{
-    margin-right: 15px;
-    padding: 10px 15px;
-    border: none;
-    border-radius: 10px;
-    color: #fff;
+.movie__button {
+  margin-right: 15px;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 10px;
+  color: #fff;
 }
 
 .movie__button-watched {
-    background-color: #1eb4c3;
+  background-color: #1eb4c3;
 }
 
 .movie__button-watched_icon {
-    width: 15px;
-    margin-left: 10px;
+  width: 15px;
+  margin-left: 10px;
 }
 
 .movie__button-delete {
-    background-color: #ce2424;
+  background-color: #ce2424;
 }
 </style>
