634ff19bbbdb12c14cf73d6cba671f43c55bc28f
Stroev D <stroev.core@gmail.com>
2023-07-06 16:58:10 +0300

    added compoisiton API style code for stores

--- src/stores/MovieStore.ts
+++ src/stores/MovieStore.ts
@@ -1,29 +1,69 @@
 import { defineStore } from "pinia";
 import { IState, IMovie } from "../types/store";
 
-export const useMovieStore = defineStore("movieStore", {
-  state: (): IState => ({
-    movies: [],
-    activeTab: 2,
-  }),
-  getters: {
-    watchedMovies(): IMovie[] {
-      return this.movies.filter((movie: IMovie) => movie.isWatched);
-    },
-    totalMoviesCounter(): number {
-      return this.movies.length;
-    },
-  },
-  actions: {
-    setActiveTab(id: number) {
-      this.activeTab = id;
-    },
-    toggleWatch(id: number) {
-      const idx = this.movies.findIndex((movie) => movie.id === id);
-      this.movies[idx].isWatched = !this.movies[idx].isWatched;
-    },
-    deleteMovie(id: number) {
-      this.movies = this.movies.filter((movie) => movie.id !== id);
-    },
-  },
+// Options API style
+
+// export const useMovieStore = defineStore("movieStore", {
+//   state: (): IState => ({
+//     movies: [],
+//     activeTab: 2,
+//   }),
+//   getters: {
+//     watchedMovies(): IMovie[] {
+//       return this.movies.filter((movie: IMovie) => movie.isWatched);
+//     },
+//     totalMoviesCounter(): number {
+//       return this.movies.length;
+//     },
+//   },
+//   actions: {
+//     setActiveTab(id: number) {
+//       this.activeTab = id;
+//     },
+//     toggleWatch(id: number) {
+//       const idx = this.movies.findIndex((movie) => movie.id === id);
+//       this.movies[idx].isWatched = !this.movies[idx].isWatched;
+//     },
+//     deleteMovie(id: number) {
+//       this.movies = this.movies.filter((movie) => movie.id !== id);
+//     },
+//   },
+// });
+
+// Composition API style
+
+import { ref, computed } from "vue";
+
+export const useMovieStore = defineStore("movieStore", () => {
+  const movies = ref([] as IMovie[]);
+  const activeTab = ref(2);
+
+  const setActiveTab = (id: number) => {
+    activeTab.value = id;
+  };
+
+  const toggleWatch = (id: number) => {
+    const idx = movies.value.findIndex((movie) => movie.id === id);
+    movies.value[idx].isWatched = !movies.value[idx].isWatched;
+  };
+
+  const deleteMovie = (id: number) => {
+    movies.value = movies.value.filter((movie) => movie.id !== id);
+  };
+
+  const watchedMovies = computed(() =>
+    movies.value.filter((movie: IMovie) => movie.isWatched)
+  );
+
+  const totalMoviesCounter = computed(() => movies.value.length);
+
+  return {
+    movies,
+    activeTab,
+    watchedMovies,
+    totalMoviesCounter,
+    toggleWatch,
+    deleteMovie,
+    setActiveTab,
+  };
 });
--- src/stores/SearchStore.ts
+++ src/stores/SearchStore.ts
@@ -5,24 +5,56 @@ import { useMovieStore } from "./MovieStore";
 const url =
   "https:api.themoviedb.org/3/search/movie?api_key=2fe29ea4e980aefdc8464fd306e20611&query=";
 
-export const useSearchStore = defineStore("searchStore", {
-  state: (): ISearchState => ({
-    loader: false,
-    movies: [],
-  }),
-  actions: {
-    async getMovies(search: string) {
-      this.loader = true;
-      const res = await fetch(`${url}${search}`);
-      const data = await res.json();
+// Options API style
 
-      this.movies = data.results;
-      this.loader = false;
-    },
-    addToUserMovies(movie: IMovie) {
-      const movieStore = useMovieStore();
-      movieStore.movies.push({ ...movie, isWatched: false });
-      movieStore.activeTab = 1;
-    },
-  },
+// export const useSearchStore = defineStore("searchStore", {
+//   state: (): ISearchState => ({
+//     loader: false,
+//     movies: [],
+//   }),
+//   actions: {
+//     async getMovies(search: string) {
+//       this.loader = true;
+//       const res = await fetch(`${url}${search}`);
+//       const data = await res.json();
+
+//       this.movies = data.results;
+//       this.loader = false;
+//     },
+//     addToUserMovies(movie: IMovie) {
+//       const movieStore = useMovieStore();
+//       movieStore.movies.push({ ...movie, isWatched: false });
+//       movieStore.activeTab = 1;
+//     },
+//   },
+// });
+
+// Composition API style
+
+import { ref } from "vue";
+export const useSearchStore = defineStore("searchStore", () => {
+  const loader = ref(false);
+  const movies = ref([] as IMovie[]);
+
+  const getMovies = async (search: string) => {
+    loader.value = true;
+    const res = await fetch(`${url}${search}`);
+    const data = await res.json();
+
+    movies.value = data.results;
+    loader.value = false;
+  };
+
+  const addToUserMovies = (movie: IMovie) => {
+    const movieStore = useMovieStore();
+    movieStore.movies.push({ ...movie, isWatched: false });
+    movieStore.activeTab = 1;
+  };
+
+  return {
+    loader,
+    movies,
+    getMovies,
+    addToUserMovies,
+  };
 });
